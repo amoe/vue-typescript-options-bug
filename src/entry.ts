747c29fdd1903fe7d37f5b286e1ec42ac050398c
David Banks <amoebae@gmail.com>
2018-03-01 11:12:54 +0000

    more useful things

--- src/entry.ts
+++ src/entry.ts
@@ -12,7 +12,13 @@ document.addEventListener("DOMContentLoaded", e => {
     });
 
     console.log("Value of component is: %o", ApplicationRoot);
+    
+    // This won't work: 
+    // TS2339: Property 'options' does not exist on type 'VueConstructor<Record<never, any> & Vue>'.
     console.log("Value of options is: %o", ApplicationRoot.options);
+
+    // Neither will this: 
+    // TS2339: Property 'created' does not exist on type 'VueConstructor<Record<never, any> & Vue>'.
     console.log("Value of created hook is: %o", ApplicationRoot.created);
 
     vueInstance.$mount('#vue-outlet');
